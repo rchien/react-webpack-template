cab1571c917c846d9b56ec6252d44fe21f294894
Richard Chien <rchien@gmail.com>
2016-07-26 06:25:41 -0700

    update templates

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/index.html',
+  filename: 'index.html',
+  inject: 'app'
+});
+
 module.exports = {
-  entry: './index',
+  entry: './src/index',
   output: {
-    filename: 'browser-bundle.js'
+    path: __dirname + '/dist',
+    filename: "bundle.js"
   },
   devtool: 'source-map',
   module: {
     loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
-        }
-      },
+      {test: /\.js$/, loader: 'babel-loader', query: {presets: ['es2015', 'react']}},
     ]
-  }
+  },
+  plugins: [HTMLWebpackPluginConfig]
 };
